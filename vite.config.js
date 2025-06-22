280a6f369663db8c17fdac5e74862bf3d2e1530a
Dave J Nicol <46354316+davejnicol@users.noreply.github.com>
2025-06-22 12:40:53 -0400

    Finished mobile/responsive menu

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,11 @@ export default defineConfig({
       rollupOptions: {
         input: {
           home: resolve(__dirname, "src/index.html"),
+          about: resolve(__dirname, "src/about-us/index.html"),
+          features: resolve(__dirname, "src/features/index.html"),
+          pricing: resolve(__dirname, "src/pricing/index.html"),
+          contact: resolve(__dirname, "src/contact-us/index.html"),
+          careers: resolve(__dirname, "src/about-us/index.html"),
           styleGuide: resolve(__dirname, "src/style-guide/index.html"),
         },
       },
